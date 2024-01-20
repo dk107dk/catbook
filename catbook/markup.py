5f35f2de272cfacb26648baf37a89f2ff378bdb4
David Kershaw <dk107dk@hotmail.com>
2024-01-20 15:41:03 -0500

    test markup

--- catbook/markup.py
+++ catbook/markup.py
@@ -1,4 +1,4 @@
-from configparser import ConfigParser
+from configparser import RawConfigParser
 from dataclasses import dataclass
 from os import path
 
@@ -12,22 +12,46 @@ class Markup:
     BLOCK: str = "|"
     QUOTED_LINE: str = '"'
     WORD_HIGHLIGHT: str = "|"
+    MARKUP_CONFIG: str = "markup.ini"
 
     def __post_init__(self):
-        self._config = ConfigParser()
+        self._config = RawConfigParser()
+        self._load_config()
+
+    def reload(self):
         self._load_config()
 
     def _load_config(self):
-        markup = "markup.ini"
-        if path.isfile(markup):
-            self._config.read(markup)
+        if path.isfile(self.MARKUP_CONFIG):
+            self._config.read(self.MARKUP_CONFIG)
             section = "markup"
-            self.JUMP = self._config[section]["jump"]
-            self.CHAPTER_TITLE = self._config[section]["chapter_title"]
-            self.BOOK_TITLE = self._config[section]["book_title"]
-            self.NEW_SECTION = self._config[section]["new_section"]
-            self.BLOCK = self._config[section]["block"]
-            self.QUOTED_LINE = self._config[section]["quoted_line"]
-            self.WORD_HIGHLIGHT = self._config[section]["word_highlight"]
+            try:
+                self.JUMP = self._config[section]["jump"]
+            except KeyError:
+                pass
+            try:
+                self.CHAPTER_TITLE = self._config[section]["chapter_title"]
+            except KeyError:
+                pass
+            try:
+                self.BOOK_TITLE = self._config[section]["book_title"]
+            except KeyError:
+                pass
+            try:
+                self.NEW_SECTION = self._config[section]["new_section"]
+            except KeyError:
+                pass
+            try:
+                self.BLOCK = self._config[section]["block"]
+            except KeyError:
+                pass
+            try:
+                self.QUOTED_LINE = self._config[section]["quoted_line"]
+            except KeyError:
+                pass
+            try:
+                self.WORD_HIGHLIGHT = self._config[section]["word_highlight"]
+            except KeyError:
+                pass
         else:
-            print(f"No {markup} file found. Using default markup markers")
+            print(f"No {self.MARKUP_CONFIG} file found. Using default markup markers")
